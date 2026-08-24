718d51adf308b430a08a23d78610d117cc61a278
Abrer19 <abrer19@gmail.com>
2026-08-24 22:41:28 +0600

    Add generic repository abstraction

---
+++ src/FutureWings.Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,10 @@
+namespace FutureWings.Infrastructure.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
