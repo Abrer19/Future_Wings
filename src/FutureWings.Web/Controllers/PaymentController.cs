5d56df4e13ad7e15da347b1fa04dd9528ec43034
md-sazid9089 <mdsazid9089@gmail.com>
2026-09-16 00:04:00 +0600

    security(payments): add authorization attribute and user claim extraction to payment controller

--- src/FutureWings.Web/Controllers/PaymentController.cs
+++ src/FutureWings.Web/Controllers/PaymentController.cs
@@ -1,13 +1,18 @@
+using System.Security.Claims;
 using FutureWings.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FutureWings.Web.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class PaymentController(IPaymentService paymentService) : ControllerBase
 {
     [HttpPost("subscription")]
-    public async Task<IActionResult> CreateSubscription(int userId, string priceId) =>
-        Ok(new { SubscriptionId = await paymentService.CreateSubscriptionAsync(userId, priceId) });
+    public async Task<IActionResult> CreateSubscription([FromQuery] string priceId) =>
+        Ok(new { SubscriptionId = await paymentService.CreateSubscriptionAsync(GetUserId(), priceId) });
+
+    private int GetUserId() => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
 }
